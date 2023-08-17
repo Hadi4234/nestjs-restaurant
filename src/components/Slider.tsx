b3c8ebab938eafbd6bb0f2ff08dc37d8ad79f3af
Hadi4234 <48425016+Hadi4234@users.noreply.github.com>
2023-08-17 17:30:03 +0600

    finish home page layout

---
+++ src/components/Slider.tsx
@@ -0,0 +1,46 @@
+"use client"
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+
+const data = [
+  {
+    id: 1,
+    title: "always fresh & always crispy & always hot",
+    image: "/slide1.png",
+  },
+  {
+    id: 2,
+    title: "we deliver your order wherever you are in NY",
+    image: "/slide2.png",
+  },
+  {
+    id: 3,
+    title: "the best pizza to share with your family",
+    image: "/slide3.jpg",
+  },
+];
+
+export default function Slider() {
+    const[currentSlide, setCurrentSlide]=useState(0)
+    useEffect(()=>{
+      const interval =setInterval(
+        ()=> setCurrentSlide((prev)=>(prev === data.length - 1 ? 0 : prev + 1)), 2000);
+        return ()=> clearInterval(interval);
+ })
+
+  return (
+    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row'>
+      {/* Text container */}
+      <div className='flex flex-1 items-center justify-center flex-col gap-8 text-red-500 font-bold bg-fuchsia-50 lg:h-full '>
+        <h1 className='text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl transition-all duration-500'>{data[currentSlide].title} </h1>
+        <button className='bg-red-500 text-white py-4 px-8 '> Order Now</button>
+      </div>
+
+      {/* Image container */}
+      <div className="w-full flex-1 relative lg:h-full">
+        <Image src={data[currentSlide].image} alt='' fill  className=' object-cover transition-all duration-500'/>
+      </div>
+      
+    </div>
+  )
+}
